2ac5b68dcccd5911910123a705282204b3731c4f
Elyorxon <elyor99-99@mail.ru>
2023-11-27 17:47:22 +0500

    js

---
+++ main.js
@@ -0,0 +1,52 @@
+//console.log(10); -это способ ввода информации в консоль браузера
+//console.log('Elyorxon'); строчный тип данных
+//console.log(24); числовой тип данный
+//console.log(true); логический тип данных true
+//console.log(false); логический тип данных false
+// var boss = 'Elyorxon';
+// var age= 23;
+// boss = 'JS'
+// console.log('My name is ' + boss + ' I am ' + age + ' years old');
+// var number = 20
+// var myWord = 'My number ' + number
+// console.log(myWord);
+
+// prompt() - создаёт окошко для ввода данных
+// prompt() - создаёт окошко для ввода данных числовой
+// alert()- создаёт нам модальное окно
+// 
+
+// // alert(str);
+// console.log('10 + 5 = ' + (10 + 5));
+// console.log('10 - 5 = ' + (10 - 5));
+// console.log('10 * 5 = ' + (10 * 5));
+// console.log('10 / 5 = ' + (10 / 5));
+
+var imya = prompt('Введите свое имя');
+console.log('Меня зовут ' + imya);
+alert   ('Здравствуйте ' + imya);
+
+
+var imya = +prompt('Введите Ваш возраст');
+console.log('Мой возраст ' + imya);
+alert('Если Вам ' + imya);
+
+var imya = prompt('Решите пример 6 + 10 =?');
+console.log('пример 1: 6 + 10 = 16 ' + 'ваш ответ ' + imya);
+alert('пример 1: 6 + 10 = 16 ' + 'ваш ответ ' + imya);
+
+var imya = prompt('Решите пример 64 - 19 =?');
+console.log('пример 2: 64 - 19 = 45 ' + 'ваш ответ ' + imya);
+alert('пример 2: 64 - 19 = 45 ' + 'ваш ответ ' + imya);
+
+var imya = prompt('Решите пример 11 * 11 =?');
+console.log('пример 3: 11 * 11 = 121 ' + 'ваш ответ ' + imya);
+alert('пример 3: 11 * 11 = 121 ' + 'ваш ответ ' + imya);
+
+var imya = prompt('Решите пример 81 / 9 =?');
+console.log('пример 4: 81 / 9 = 9 ' + 'ваш ответ ' + imya);
+alert('пример 4: 81 / 9 = 9 ' + 'ваш ответ ' + imya);
+
+var imya = prompt('Решите пример 20 % 9 =?');
+console.log('пример 4: 20 % 9 = 2 ' + 'ваш ответ ' + imya);
+alert('пример 4: 20 % 9 = 2 ' + 'ваш ответ ' + imya);
